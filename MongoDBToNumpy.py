47ff80f8ddd017cbee48073c7b417a458350de2f
evinceng <aslan.evin@gmail.com>
2018-04-26 15:21:58 +0200

    mongodb trials

---
+++ MongoDBToNumpy.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 10:26:38 2018
+
+@author: evin
+"""
+
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+import sys
+import pprint
+import matplotlib.pyplot as plt
+import numpy as np
+
+def insertDocs():
+    docs = [
+            {"timeStamp":"25.09.2017 18:06:36.7452","leftPos":{"x":"-16,7315728269386","y":"3,89062108428363","z":"56,2198678361344"},"rightPos":{"x":"-10,0026425550767","y":"3,29426110476243","z":"56,0879127434864"},"leftGaze":{"x":"-3,49850843959982","y":"22,1567167927533","z":"6,37821179543316"},"rightGaze":{"x":"-7,89889571217705","y":"21,4349212191887","z":"6,11551553472114"},"leftPupilDiameter":"3,069977","rightPupilDiameter":"2,963455"},
+            {"timeStamp":"25.09.2017 18:06:36.7943","leftPos":{"x":"-16,7098222900898","y":"3,90436146663274","z":"56,2242540276036"},"rightPos":{"x":"-9,98631284785558","y":"3,30704196490619","z":"56,0891633566908"},"leftGaze":{"x":"-1,72234459305309","y":"22,4207459567531","z":"6,47430475671185"},"rightGaze":{"x":"-5,62868343709524","y":"22,422401237002","z":"6,47490719311872"},"leftPupilDiameter":"3,102768","rightPupilDiameter":"3,005875"},
+            {"timeStamp":"25.09.2017 18:06:36.8864","leftPos":{"x":"-16,9001924206929","y":"3,88461160788982","z":"56,1994065129416"},"rightPos":{"x":"-10,1420321126478","y":"3,34486136857306","z":"56,0834237367933"},"leftGaze":{"x":"-0,363629186618618","y":"24,2620824531458","z":"7,14445601564266"},"rightGaze":{"x":"-4,504263384347","y":"23,2056102294919","z":"6,75995476228995"},"leftPupilDiameter":"3,104858","rightPupilDiameter":"3,024307"},
+            {"timeStamp":"25.09.2017 18:06:36.9334","leftPos":{"x":"-16,9407320929185","y":"3,87290156474623","z":"56,2004726303334"},"rightPos":{"x":"-10,1813528995491","y":"3,34311107269234","z":"56,0867164621378"},"leftGaze":{"x":"0,0382847428729292","y":"24,1641581806759","z":"7,10881664467265"},"rightGaze":{"x":"-4,45922413548337","y":"23,5984198978849","z":"6,90291716721133"},"leftPupilDiameter":"3,072189","rightPupilDiameter":"3,016708"}
+    ]    
+    collection.insert_many(docs)
+
+if __name__ == "__main__":
+    try:
+        client = MongoClient(host="localhost", port=27017)
+    except ConnectionFailure, e:
+        sys.stderr.write("Could not connect to MongoDB: %s" % e)
+        sys.exit(1)
+        
+    dbh = client["local"]
+    collection = dbh["sensor"]
+    #insertDocs()
+    projection = {
+            "_id": 0,
+            "timeStamp": 1,
+            "leftPupilDiameter": 1
+            }
+    rm = list(collection.find({}, projection))
+    
+    #pprint.pprint(rm[0])
+    timeStamps = [sensor["timeStamp"] for sensor in rm]
+    leftPupilDiameters = [sensor["leftPupilDiameter"] for sensor in rm]
+    
+    timeStampsnpArray = np.array(timeStamps)
+    leftPupilDiametersnpArray = np.array(leftPupilDiameters)
+    
+    plt.clf()
+    fig, ax = plt.subplots()
+    
+    ax.scatter(timeStampsnpArray, leftPupilDiametersnpArray, alpha = 0.5)
+    plt.show()
+    
+    
+    
--- Test.py
+++ Test.py
@@ -16,31 +16,59 @@ import time
 import datetime
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+import json
 
-def insertTimeCollection():
-    try:
-        client = MongoClient(host="localhost", port=27017)
-    except ConnectionFailure, e:
-        sys.stderr.write("Could not connect to MongoDB: %s" % e)
-        sys.exit(1)
-        
+def insertTimeCollection(client):
     dbh = client["local"]
-    collection = dbh.time
+    collection = dbh["time"]
     
     for i in range (0,100):
         rawTime = time.time()
         timeStamp = datetime.datetime.fromtimestamp(rawTime).strftime('%Y-%m-%d_%H%M%S')
         collection.insert_one({"timeStamp" : rawTime, "hrTimeStamp": timeStamp})
-        time.sleep(0.5)
+        time.sleep(0.01)
         
     print "Successfully inserted documents"
-
-def main():
-    insertTimeCollection()
-
-if __name__ == "__main__":
-    main()
     
+def insertUser(client):
+    dbh = client["test"]
+    collection = dbh["user"]
+    ret = collection.find_one({'userName': 'evina'})
+    if ret == None:
+        collection.insert({'userName': 'evina',
+                           'password': '',
+                           'sessions': [1]})
+    
+def main(client):
+    #insertTimeCollection()
+    #insertUser(client)
+    pass
+
+def shapeData(data):
+    pass
+    
+if __name__ == "__main__":
+    try:
+        client = MongoClient(host="localhost", port=27017)
+    except ConnectionFailure, e:
+        sys.stderr.write("Could not connect to MongoDB: %s" % e)
+        sys.exit(1)
+    #main(client)
+    data = '{"tobiiEyeTracker":{"timeStamp":"30.12.2015 14:06:20.2412","leftPos":{"x":"-0,228793755914194","y":"11,5027813555582","z":"60,912982163767"},"rightPos":{"x":"5,89524352818696","y":"11,2245013358383","z":"61,0730322352786"},"leftGaze":{"x":"3,15812377150551","y":"17,3247499470179","z":"4,61986983600664"},"rightGaze":{"x":"-2,49937069615642","y":"17,3932511520527","z":"4,64480229580618"},"leftPupilDiameter":"2,645874","rightPupilDiameter":"2,622345"}}'
+    sensorMessage=[]
+    sensorMessage.append(json.loads(data))
+    #print sensorMessage
+    dbh = client["local"]
+    collection = dbh["sensor"]
+    dataDict = {}
+    dataDict["timeStamp"] = "30.12.2015 14:06:20.2412"
+    dataDict["leftPos"] = {"x":"-0,228793755914194","y":"11,5027813555582","z":"60,912982163767"}
+    
+    data = data.replace('{"tobiiEyeTracker":', '')
+    data = data[:-1]
+    print data
+    collection.insert_one(json.loads(data))
+    #print double("-0,228793755914194")
 #from pymongo import MongoClient
 #import time
 #import datetime
